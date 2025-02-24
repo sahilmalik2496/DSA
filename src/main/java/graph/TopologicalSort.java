92ab520d704f2145a621d2c6a6ce1e922cef6e59
Sahil <imsahil2496@gmail.com>
2025-02-24 09:21:41 +0530

    Update TopologicalSort.java

--- src/main/java/graph/TopologicalSort.java
+++ src/main/java/graph/TopologicalSort.java
@@ -4,6 +4,67 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        int n= prerequisites.length;
+        for(int i=0; i< numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+
+        int ind[] = new int[numCourses];
+        for(int i=0; i< n; i++) {
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        for(int i=0; i< numCourses; i++) {
+            System.out.println(i);
+            for(int it: adj.get(i)) {
+                ind[it]++;
+                System.out.print(it + " ");
+                System.out.println(ind[it]);
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0; i< numCourses; i++) {
+            if (ind[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int top[] = new int[numCourses];
+        int count =0;
+        while(!q.isEmpty()) {
+            int node = q.poll();
+            top[count++] = node;
+
+            for(int it: adj.get(node)) {
+                ind[it]--;
+                if(ind[it] == 0) {
+                    q.offer(it);
+                }
+            }
+        }
+
+        if (count == numCourses) {
+            return top;
+        }
+
+        return new int[0];
+    }
+}
+*/
+
 public class TopologicalSort {
         public boolean canFinish(int numCourses, int[][] prerequisites) {
             ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
