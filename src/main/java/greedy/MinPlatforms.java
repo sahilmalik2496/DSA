8746f6c608da2602d5b2d11c7833c7fdbd6be623
Sahil <imsahil2496@gmail.com>
2025-03-20 09:39:40 +0530

    Create MinPlatforms.java

---
+++ src/main/java/greedy/MinPlatforms.java
@@ -0,0 +1,41 @@
+package greedy
+
+/*
+Problem Statement: We are given two arrays that represent the arrival and departure times of trains that stop at the platform. We need to find the minimum number of platforms needed at the railway station so that no train has to wait.
+
+Examples 1:
+
+Input: N=6, 
+arr[] = {9:00, 9:45, 9:55, 11:00, 15:00, 18:00} 
+dep[] = {9:20, 12:00, 11:30, 11:50, 19:00, 20:00}
+
+Output:3
+*/
+public class MinPlatforms {
+static int findPlatform(int arr[], int dep[], int n)
+    {
+        Arrays.sort(arr); 
+        Arrays.sort(dep); 
+  
+        int plat_needed = 1, result = 1; 
+        int i = 1, j = 0; 
+  
+        while (i < n && j < n) { 
+        
+            if (arr[i] <= dep[j]) { 
+                plat_needed++; 
+                i++; 
+            } 
+  
+            else if (arr[i] > dep[j]) { 
+                plat_needed--; 
+                j++; 
+            } 
+  
+            if (plat_needed > result) 
+                result = plat_needed; 
+        } 
+  
+        return result; 
+    }
+}
