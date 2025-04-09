bf43691ac575ced927b8ca1595ce313f0eb008fd
Sahil <imsahil2496@gmail.com>
2025-04-09 10:07:29 +0530

    Create LevelOrder.java

---
+++ src/main/java/binaryTree/LevelOrder.java
@@ -0,0 +1,34 @@
+package binaryTree;
+
+/*
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+*/
+
+
+class LevelOrder {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        q.add(root);
+        while(!q.isEmpty()) {
+            int n = q.size();
+            System.out.println(n);
+            List<Integer> list = new ArrayList<>();
+            for(int i=0; i< n; i++){
+                TreeNode curr = q.poll();
+                list.add(curr.val);
+                if(curr.left != null) {
+                    q.add(curr.left);
+                } 
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
