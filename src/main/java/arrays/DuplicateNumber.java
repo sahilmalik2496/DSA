a64a97e273369ad7a3c8ceeb11e077e6ea48480a
sahil.malik <imsahil2496@gmail.com>
2025-05-04 12:50:39 +0530

    DSA

--- src/main/java/arrays/DuplicateNumber.java
+++ src/main/java/arrays/DuplicateNumber.java
@@ -1,9 +1,9 @@
 package arrays;
 
 /*
-Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
-There is only one repeated number in nums, return this repeated number.
-You must solve the problem without modifying the array nums and using only constant extra space.
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+There is only one repeated number in nums, return this repeated number.
+You must solve the problem without modifying the array nums and using only constant extra space.
 
  */
 public class DuplicateNumber {
