cf38cab72c4d2d628731316258735a918d891192
sahil.malik <imsahil2496@gmail.com>
2025-09-14 20:26:17 +0530

    DSA

--- src/main/java/graph/CriticalConnections.java
+++ src/main/java/graph/CriticalConnections.java
@@ -3,7 +3,17 @@ package graph;
 import java.util.*;
 
 /*
-This algorithm efficiently finds bridges in an undirected graph using DFS traversal and maintaining discovery time and lowest reachable time for each node.
+There are n servers numbered from 0 to n - 1 connected by undirected server-to-server connections forming
+a network where connections[i] = [ai, bi] represents a connection between servers ai and bi. Any server can reach
+ other servers directly or indirectly through the network.
+
+A critical connection is a connection that, if removed, will make some servers unable to reach some other server.
+
+Return all critical connections in the network in any order.
+
+
+This algorithm efficiently finds bridges in an undirected graph using DFS traversal and maintaining discovery time
+ and lowest reachable time for each node.
 
 Key Idea
 Perform DFS traversal while keeping track of:
