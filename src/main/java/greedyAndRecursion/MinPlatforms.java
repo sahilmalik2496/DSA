9cac8ea2f9d106a539ea6b1f6a41fd1b13c8ff6a
sahil.malik <imsahil2496@gmail.com>
2025-10-13 13:09:02 +0530

    dsa

--- src/main/java/greedyAndRecursion/MinPlatforms.java
+++ src/main/java/greedyAndRecursion/MinPlatforms.java
@@ -3,7 +3,8 @@ package greedyAndRecursion;
 import java.util.Arrays;
 
 /*
-Problem Statement: We are given two arrays that represent the arrival and departure times of trains that stop at the platform. We need to find the minimum number of platforms needed at the railway station so that no train has to wait.
+Problem Statement: We are given two arrays that represent the arrival and departure times of trains that stop at the
+platform. We need to find the minimum number of platforms needed at the railway station so that no train has to wait.
 
 Examples 1:
 
