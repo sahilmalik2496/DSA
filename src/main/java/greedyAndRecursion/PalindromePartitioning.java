435395e1d1220a0ffcdaa3f31055dfa0702e1ac3
sahil.malik <imsahil2496@gmail.com>
2025-11-26 08:51:24 +0530

    dsa

--- src/main/java/greedyAndRecursion/PalindromePartitioning.java
+++ src/main/java/greedyAndRecursion/PalindromePartitioning.java
@@ -3,6 +3,7 @@ package greedyAndRecursion;
 import java.util.*;
 
 /*
+https://leetcode.com/problems/palindrome-partitioning/description/
 Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome
  partitioning of s.
 
--- src/main/java/greedyAndRecursion/Permutations.java
+++ src/main/java/greedyAndRecursion/Permutations.java
@@ -3,6 +3,8 @@ package greedyAndRecursion;
 import java.util.*;
 
 /*
+https://leetcode.com/problems/permutations/description/
+
 Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 
  
