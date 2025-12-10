6aa4658f61f22a0cb332b11f6259ecce50fa91d4
sahil.malik <imsahil2496@gmail.com>
2025-12-10 21:07:49 +0530

    dsa

---
+++ src/main/java/arrays/TrappingRainWater.java
@@ -0,0 +1,73 @@
+package arrays;
+
+/*
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it
+ can trap after raining.
+
+
+
+Example 1:
+
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6
+units of rain water (blue section) are being trapped.
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+
+
+Constraints:
+
+n == height.length
+1 <= n <= 2 * 104
+0 <= height[i] <= 105
+ */
+public class TrappingRainWater {
+    public int trap(int[] height) {
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+        left[0] = height[0];
+        right[height.length - 1] = height[height.length - 1];
+        int maxLeft = Integer.MIN_VALUE;
+        int maxRight = Integer.MIN_VALUE;
+        for (int i = 1; i < height.length; i++) {
+            left[i] = Math.max(left[i - 1], height[i]);
+        }
+        for (int i = height.length - 2; i >= 0; i--) {
+            right[i] = Math.max(right[i + 1], height[i]);
+        }
+        int res = 0;
+        for (int i = 0; i < height.length; i++) {
+            res += Math.min(left[i], right[i]) - height[i];
+        }
+        return res;
+    }
+}
+/*
+class Solution {
+    public int trap(int[] height) {
+        int size = height.length;
+        int maxLeft = Integer.MIN_VALUE;
+        int maxRight = Integer.MIN_VALUE;
+        int res =0;
+        int left =0, right = size-1;
+        while(left < right) {
+            if(height[left]< height[right]){
+                if (height[left] >= maxLeft)
+                    maxLeft = height[left];
+                else res += maxLeft - height[left];
+                left ++;
+            } else {
+                if(height[right] >= maxRight)
+                    maxRight = height[right];
+                    else res += maxRight - height[right];
+            right--;
+                }
+        }
+        return res;
+    }
+}
+ */
